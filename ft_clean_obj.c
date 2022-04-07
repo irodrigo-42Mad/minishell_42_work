85e04d79f8880fdd8cdab8fde2e6ab588bfd4d59
irodrigo-42Mad <irodrigo@student.42madrid.com>
2022-04-07 10:24:48 +0200

    clean correct

--- ft_clean_obj.c
+++ ft_clean_obj.c
@@ -12,19 +12,30 @@
 
 #include "minishell.h"
 
-void ft_clean_instr(t_lst *instr)
+void ft_clean_instr()
 {
 	t_lst *tmp;
-	
-	if (instr == NULL)
+
+	if (g_ms->instr == NULL)
     	return ;
-	tmp = instr;
-	while (tmp->next != NULL)
+	tmp = g_ms->instr;
+	while (g_ms->instr != NULL)
 	{
-   		instr = tmp->next;
-   		free (tmp);
-   		tmp = instr;
+		tmp = g_ms->instr->next;
+		free(g_ms->instr->str_cmd);
+		g_ms->instr->str_cmd = NULL;
+		g_ms->instr->file_in = 0;
+		g_ms->instr->file_out = 0;
+		g_ms->instr->exe_state = 0;
+		free(g_ms->instr->str_line);
+		free(g_ms->instr->str_aux);
+		free(g_ms->instr->str_save);
+		free(g_ms->instr->str_aux_save);
+		ft_bzero(g_ms->instr, sizeof(t_lst));
+   		free (g_ms->instr);
+		g_ms->instr = tmp;
 	}
 	free (tmp);
-	instr = NULL;
+	tmp = NULL;
+	g_ms->instr = NULL;
 }
--- ft_cleaning.c
+++ ft_cleaning.c
@@ -1,16 +1,26 @@
 #include "minishell.h"
 
-void ft_pre_clean(t_ms *g_ms)
+void ft_pre_clean(void)
 {
-	if (g_ms->sh_env)
-		free_matrix(g_ms->sh_env);
-	if (g_ms->sh_envar)
-	 	free_vars(g_ms->sh_envar);
-	free(g_ms->prompt);
-	g_ms->argc = 0;
 	g_ms->state = 0;
 	g_ms->flg_err = 0;
-	if (g_ms->instr)
-		free_nodes(g_ms->instr);
-	ft_bzero(g_ms, sizeof(t_ms));
+	free(g_ms->prompt);
+	g_ms->prompt = NULL;
+	ft_clean_instr();
+	g_ms->prcs_n = 0;
+	g_ms->pars_cmd = NULL;
+	free (g_ms->str);
+	g_ms->str = NULL;
+	g_ms->to_clean = NULL;
+
+	// if (g_ms->sh_env)
+	// 	free_matrix(g_ms->sh_env);
+	// if (g_ms->sh_envar)
+	//  	free_vars(g_ms->sh_envar);
+	// free(g_ms->prompt);
+	// g_ms->argc = 0;
+	g_ms->state = 0;
+	g_ms->flg_err = 0;
+
+	// ft_bzero(g_ms, sizeof(t_ms));
 }
