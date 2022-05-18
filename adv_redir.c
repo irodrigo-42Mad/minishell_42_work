3657834d5e6d6a2f5c217a5f4ad1bf354220d84f
Manuel Grau <mgrau@c3r2s5.42madrid.com>
2022-05-18 09:40:06 +0200

    adv redir modified

--- adv_redir.c
+++ adv_redir.c
@@ -3,115 +3,134 @@
 /*                                                        :::      ::::::::   */
 /*   adv_redir.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hatman <hatman@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mgrau <mgrau@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 13:22:45 by irodrigo          #+#    #+#             */
-/*   Updated: 2022/05/17 13:00:23 by hatman           ###   ########.fr       */
+/*   Updated: 2022/05/18 09:39:23 by mgrau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//tenemos que trabajar sobre las redirecciones a ver cual es el problema
-
-//tenemos que trabajar en la forma en la que tomamos los elementos entre "" para funcionar
-
-void	ft_check_redir(t_lst *lst)
+void ft_check_redir(t_lst *lst)
 {
-	char	*st_aux;
-	size_t	pos;
+ char *st_aux;
+ size_t pos;
 
-	pos = 0;
-	//st_aux = lst->str_cmd;
-	while (lst->exe_state == SUCCESS && lst->str_cmd[pos] != '\0')
-	//while (lst->exe_state == SUCCESS && *st_aux != '\0')
-	{
-		if (lst->str_cmd[pos] == '>' || lst->str_cmd[pos] == '<')
-		//if (*st_aux == '>' || *st_aux == '<')
-		{
-			if (lst->str_cmd[pos + 1] == '>')
-			//if (*(st_aux + 1) == '>')
-			{
-				pos += 2;
-				st_aux = ft_calloc((ft_strlen(lst->str_cmd) - pos) + 1,
-						sizeof(char));
-				ft_strnlcpy(st_aux, lst->str_cmd, pos,
-					(ft_strlen(lst->str_cmd) - pos));
-				//st_aux += 2;
-				//ft_redir(,lst,FD_OUT, MULTIPLE);
-				ft_redir(&st_aux, lst, FD_OUT, MULTIPLE);
-			}
-			else
-			{
-				if (lst->str_cmd[pos + 1] == '>')
-				{
-					pos++;
-					st_aux = ft_calloc((ft_strlen(lst->str_cmd) - pos) + 1,
-							sizeof(char));
-					ft_redir(&st_aux, lst, FD_OUT, UNIQUE);
-				}
-				else
-				{
-					//size_t	mlen;
-					//mlen = (ft_strlen(lst->str_cmd) - pos) + 1;
-					st_aux = ft_calloc((ft_strlen(lst->str_cmd) - pos) + 1,
-							sizeof(char));
-					ft_strnlcpy(st_aux, lst->str_cmd, pos,
-						(ft_strlen(lst->str_cmd) - pos));
-					ft_redir(&st_aux, lst, FD_OUT, UNIQUE);
-				}
-			// 	if (*st_aux++ == '>')
-			// 		ft_redir(&st_aux, lst, FD_OUT, UNIQUE);
-			// 	else
-			// 		ft_redir(&st_aux, lst, FD_IN, UNIQUE);
-			}
-		}
-		else
-			pos++;
-			//st_aux++;
-	}
+ pos = 0;
+ while (lst->exe_state == SUCCESS && lst->str_aux[pos] != '\0')
+ {
+  if (lst->str_aux[pos] == '>' || lst->str_aux[pos] == '<')
+  {
+   if (lst->str_aux[pos + 1] == '>')
+   {
+    pos+= 2;
+    //realizar una cadena que tome la posicion del caracter
+    // necesario para tomar el nombre de la redireccion
+    //
+
+    st_aux = ft_calloc((ft_strlen(lst->str_aux) - pos) + 1,
+       sizeof(char));
+    // aqui se obtiene el nombre del fichero de salida
+    // si está entre "" obtendremos nombre igual
+
+
+    if (lst->str_aux[pos] != '\"')
+     ft_strnlcpy(st_aux, lst->str_aux, pos,
+      (ft_strlen(lst->str_aux) - pos));
+    else
+     ft_strnlcpy(st_aux, lst->str_line, pos,
+      (ft_strlen(lst->str_aux) - pos));
+
+//pos = ft_take_fname(lst, pos,)
+    ft_control_out_mode(st_aux,lst,APPEND);
+
+    //st_aux += 2;
+    //ft_redir(,lst,FD_OUT, MULTIPLE);
+    //ft_redir(&st_aux, lst, FD_OUT, MULTIPLE);
+   }
+   else
+   {
+     if (lst->str_aux[pos + 1] == '>')
+    {
+     pos++;
+     st_aux = ft_calloc((ft_strlen(lst->str_aux) - pos) + 1,
+       sizeof(char));
+
+     // tomar el nombre del fichero
+
+//pos = ft_take_fname(lst, pos,)
+      ft_control_out_mode(st_aux, lst, TRUNCATE);
+     //ft_redir(&st_aux, lst, FD_OUT, UNIQUE);
+    }
+    else
+    {
+     //pos = ft_take_fname(lst, pos,)
+
+
+
+     size_t mlen;
+     mlen = (ft_strlen(lst->str_aux) - pos) + 1;
+     st_aux = ft_calloc((ft_strlen(lst->str_aux) - pos) + 1,
+       sizeof(char));
+     pos+= 2;
+
+     //ft_strnlcpy(st_aux, lst->str_aux, pos,
+     // (ft_strlen(lst->str_aux) - pos));
+    if (lst->str_aux[pos] != '\"')
+     ft_strnlcpy(st_aux, lst->str_aux, pos,
+      (ft_strlen(lst->str_aux) - pos));
+    else
+     ft_strnlcpy(st_aux, lst->str_line, pos,
+      (ft_strlen(lst->str_aux) - pos));
+
+     ft_indirection(st_aux, lst);
+     //ft_redir(&st_aux, lst, FD_OUT, UNIQUE);
+
+    }
+   //  if (*st_aux++ == '>')
+   //   ft_redir(&st_aux, lst, FD_OUT, UNIQUE);
+   //  else
+   //   ft_redir(&st_aux, lst, FD_IN, UNIQUE);
+   }
+  }
+  else
+   pos++;
+ }
 }
 // revisar que tenemos que hacer con las redirecciones.
 
-char	*ft_obtain_file(t_lst *lst, char **str_line)
-{
-	char	*aux;
-	char	*file;
-	int		len;
 
-	//len = string_length_bash(*str_line, SUCCESS);
-	len = ft_set_lenv(*str_line, PARAM);
-	//add_envar_len(&len, *str_line, PARAM);
-	file = malloc(sizeof(char) * (len + 1));
-	aux = file;
-	ft_set_strandenv(&aux, str_line, lst, &len);
-	//write_str_w_envar(str_line, &aux, &len, lst);
-	return (file);
+char *ft_obtain_file(t_lst *lst, char **str_line)
+{
+ char *aux;
+ char *file;
+ int  len;
+
+ //len = string_length_bash(*str_line, SUCCESS);
+ len = ft_set_lenv(*str_line, PARAM);
+ //add_envar_len(&len, *str_line, PARAM);
+ file = malloc(sizeof(char) * (len + 1));
+ aux = file;
+ ft_set_strandenv(&aux, str_line, lst, &len);
+ //write_str_w_envar(str_line, &aux, &len, lst);
+ return (file);
 }
 
-void	ft_redirections(void)
+void ft_redirections(void)
 {
-	int		i;
-	int		tot_proc;
-	t_lst	*elm;
+ int  i;
+ t_lst *elm;
 
-	i = 0;
-	tot_proc = g_ms->prcs_n;
-	elm = g_ms->instr;
-	//elm = s->instr;
-	while (i++ < tot_proc)
-	{
-		if (elm->exe_state == SUCCESS)
-		{
-			if (elm->type == 1)
-			{
-				ft_check_redir(elm);
-			//ft_clean_hdoc_elm(elm);
-			//redirection_checker(node);
-			//clean_hdoc_bar(node);
-			}
-		}
-		elm = elm->next;
-		// instr anterior (esto puede generar leaks de memoria)
-	}
+ i = 0;
+ elm = g_ms->instr;
+ while (i++ < g_ms->prcs_n)
+ {
+  if (elm->exe_state == SUCCESS)
+  {
+   ft_check_redir(elm);    // revisando este de aquí
+   ft_clean_hdoc_pos(elm); // este funciona ok
+  }
+  elm = elm->next;
+ }
 }
--- ft_exec_redir.c
+++ ft_exec_redir.c
@@ -6,33 +6,33 @@
 /*   By: mgrau <mgrau@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 11:46:12 by irodrigo          #+#    #+#             */
-/*   Updated: 2022/05/17 12:04:55 by mgrau            ###   ########.fr       */
+/*   Updated: 2022/05/18 09:38:43 by mgrau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <dirent.h> 
 
-int		ft_dircheck(char *file)
+int  ft_dircheck(char *file)
 {
-	DIR  *rst_dir;
+ DIR  *rst_dir;
 
-	rst_dir = opendir(file);
-	if (rst_dir)
-	{
-		closedir(rst_dir);
-		return (ft_element_error(file, -126));
-	}
-	return (0);
+ rst_dir = opendir(file);
+ if (rst_dir)
+ {
+  closedir(rst_dir);
+  return (ft_element_error(file, -126));
+ }
+ return (0);
 }
 
 
 
 
-// void	ft_redir(char **str, t_lst *lst, int dir, int mode)
+// void ft_redir(char **str, t_lst *lst, int dir, int mode)
 // {
-// 	int		new_pos;
-// 	char	*file;
+//  int  new_pos;
+//  char *file;
 
 
 
@@ -40,71 +40,71 @@ int		ft_dircheck(char *file)
 
 
 
-// 	//ft_set_strbegin();
+//  //ft_set_strbegin();
 
-// 	//place_str_pointers(aux, &lst->line_aux, &lst->line, red);
-// 	file = *str;
+//  //place_str_pointers(aux, &lst->line_aux, &lst->line, red);
+//  file = *str;
 
-// 	//lst->str_line = ft_strdup(lst->str_cmd);
-// 	file = ft_obtain_file(lst, &lst->str_line);
-// 	if (lst->exe_state == SUCCESS)
-// 	{
-// 		if (mode == FD_OUT && dir == MULTIPLE)
-// 			ft_control_mode(file, lst, APPEND);
-// 		else if (mode == FD_OUT && dir == UNIQUE)
-// 			ft_control_mode(file, lst, TRUNCATE);
-// 		else
-// 			ft_new_input(file, lst);
-// 	}
-// 	free(file);
-// 	new_pos = lst->str_line - lst->str_save;
-// 	new_pos -= (lst->str_aux - lst->str_aux_save);
+//  //lst->str_line = ft_strdup(lst->str_cmd);
+//  file = ft_obtain_file(lst, &lst->str_line);
+//  if (lst->exe_state == SUCCESS)
+//  {
+//   if (mode == FD_OUT && dir == MULTIPLE)
+//    ft_control_mode(file, lst, APPEND);
+//   else if (mode == FD_OUT && dir == UNIQUE)
+//    ft_control_mode(file, lst, TRUNCATE);
+//   else
+//    ft_new_input(file, lst);
+//  }
+//  free(file);
+//  new_pos = lst->str_line - lst->str_save;
+//  new_pos -= (lst->str_aux - lst->str_aux_save);
 
-// 	// ahi que mirar como movemos la position
-// 	ft_locate_strptr(new_pos, &lst->str_aux, str);
-// 	//move_str_pointers(new_pos, &lst->str_aux, str);
+//  // ahi que mirar como movemos la position
+//  ft_locate_strptr(new_pos, &lst->str_aux, str);
+//  //move_str_pointers(new_pos, &lst->str_aux, str);
 // }
 
-void	ft_control_out_mode(char *file, t_lst *lst, int mode)
+void ft_control_out_mode(char *file, t_lst *lst, int mode)
 {
-	if (lst->file_out != 1)
-			close(lst->file_out);
-	if (ft_dircheck(file) < 0)
-		return ;
-	if (mode == APPEND)
-	{
-		if(!access(file, W_OK))
-			lst->file_out = open(file, O_RDWR| O_APPEND);
-		else
-			lst->file_out = open(file, O_RDWR| O_CREAT, S_IRWXU);
-	}
-	else if (mode == TRUNCATE)
-	{
-		if (!access(file, R_OK))
-			unlink(file);
-		lst->file_out = open(file, O_RDWR| O_CREAT, S_IRWXU);
-	}
-	if (lst->file_out == -1)
-		ft_write_file_err(lst, file);
+ if (lst->file_out != 1)
+   close(lst->file_out);
+ if (ft_dircheck(file) < 0)
+  return ;
+ if (mode == APPEND)
+ {
+  if(!access(file, W_OK))
+   lst->file_out = open(file, O_RDWR| O_APPEND);
+  else
+   lst->file_out = open(file, O_RDWR| O_CREAT, S_IRWXU);
+ }
+ else if (mode == TRUNCATE)
+ {
+  if (!access(file, R_OK))
+   unlink(file);
+  lst->file_out = open(file, O_RDWR| O_CREAT, S_IRWXU);
+ }
+ if (lst->file_out == -1)
+  ft_write_file_err(lst, file);
 }
 
-void	ft_indirection(char *file, t_lst *lst)
+void ft_indirection(char *file, t_lst *lst)
 {
-	//fd = check_for_hdoc_priority(lst->str_line);
-	if (!ft_dircheck(file))
-	{
-		if (lst->herename)
-			// si es el último heredoc, debemos llenar el archivo con el contenido de este
-			// de forma correcta.
-		//	unlink_one_heredoc(&(node->hdoc_name));
+ //fd = check_for_hdoc_priority(lst->str_line);
+ if (!ft_dircheck(file))
+ {
+  if (lst->herename)
+   // si es el último heredoc, debemos llenar el archivo con el contenido de este
+   // de forma correcta.
+  // unlink_one_heredoc(&(node->hdoc_name));
 
 
-		// el hdoc estará ya creado, lo que es necesario es ajustarlo correctamente
-		// en el los ficheros en los que tiene que aparecer
-		if (!access(file, R_OK))
-			ft_write_file_err(lst, file);
-		lst->file_in = open(file, O_RDONLY);
-		if (lst->file_in == -1)
-			ft_write_file_err(lst, file);
-	}
+  // el hdoc estará ya creado, lo que es necesario es ajustarlo correctamente
+  // en el los ficheros en los que tiene que aparecer
+  if (!access(file, R_OK))
+   ft_write_file_err(lst, file);
+  lst->file_in = open(file, O_RDONLY);
+  if (lst->file_in == -1)
+   ft_write_file_err(lst, file);
+ }
 }
