db9b08932c2244bad3613c2c98270e7ad8b6ad03
Manuel Grau <mgrau@c3r2s5.42madrid.com>
2022-05-18 10:51:09 +0200

    pathfinder issue solved

--- pathfinder.c
+++ pathfinder.c
@@ -6,7 +6,7 @@
 /*   By: mgrau <mgrau@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 12:14:41 by mgrau             #+#    #+#             */
-/*   Updated: 2022/04/29 12:16:42 by mgrau            ###   ########.fr       */
+/*   Updated: 2022/05/18 10:48:57 by mgrau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,19 @@ char	*get_pathname(char *arg)
 		return (ft_strdup(arg));
 	dest = NULL;
 	paths = get_pathlocation(g_ms->sh_env);
-	i = create_probable_str(&dest, arg, paths, i);
-	while ((access(dest, X_OK)) == -1)
+	if (paths)
 	{
-		if (i >= strlen(paths) - 5)
-		{
-			free(dest);
-			return (NULL);
-		}
-		free(dest);
 		i = create_probable_str(&dest, arg, paths, i);
+		while ((access(dest, X_OK)) == -1)
+		{
+			if (i >= strlen(paths) - 5)
+			{
+				free(dest);
+				return (NULL);
+			}
+			free(dest);
+			i = create_probable_str(&dest, arg, paths, i);
+		}
 	}
 	return (dest);
 }
@@ -42,7 +45,7 @@ char	*get_pathlocation(char **envp)
 	int	i;
 
 	i = 0;
-	while ((ft_strncmp("PATH=", envp[i], 5)))
+	while (envp[i] && (ft_strncmp("PATH=", envp[i], 5)))
 		i++;
 	return (envp[i]);
 }
