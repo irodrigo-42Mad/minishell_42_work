eeb70d0f575e46c9f62923796eaeca234859c65e
Manuel Grau <mgrau@c3r7s2.42madrid.com>
2022-06-07 10:01:59 +0200

    engine

--- ft_hdoc_engine.c
+++ ft_hdoc_engine.c
@@ -6,100 +6,75 @@
 /*   By: mgrau <mgrau@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 13:20:03 by mgrau             #+#    #+#             */
-/*   Updated: 2022/06/03 08:48:05 by mgrau            ###   ########.fr       */
+/*   Updated: 2022/06/03 11:00:31 by mgrau            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-/*
-void	ft_set_hdoc(t_lst *lst)
-{
-	char	*tmp;
 
+void	hdoc_strings(t_lst *lst)
+{
 	lst->str_aux = ft_strdup(lst->str_cmd);
 	lst->str_save = ft_strdup(lst->str_line);
-	tmp = lst->str_aux;
-	while (*tmp != '\0')
-	{
-		tmp = ft_strnstr(tmp, "<<", ft_strlen(tmp));
-		if (tmp == NULL)
-			break ;
-		else
-		{
-			tmp += 2;
-			ft_set_strpntr(&tmp, &lst->str_aux, &lst->str_line, 2);
-			ft_clear_str(tmp, lst);
-		}
-		lst->herename = ft_eofcatch(&lst->here_num, &lst->str_line);
-		heredoc_opener(lst->herename);
-	}
-	if (lst->file_in != FD_IN)
-		close(lst->file_in);
-	lst->file_in = 0;
-	ft_hdc_prepareln(lst);
-}*/
+	lst->str_aux_save = ft_strdup(lst->str_line);
+}
 
 void	ft_set_hdoc(t_lst *lst)
 {
 	char	*tmp;
 	int		quotes;
 	int		begin;
-	int		len;
-	lst->str_aux = ft_strdup(lst->str_cmd);
-	lst->str_save = ft_strdup(lst->str_line);
-	lst->str_aux_save = ft_strdup(lst->str_line);
+
+	hdoc_strings(lst);
 	tmp = lst->str_save;
 	quotes = 0;
 	begin = 0;
 	while (tmp[begin] != '\0')
 	{
-		begin = ft_strlen(lst->str_aux) - ft_strlen(ft_strnstr(tmp, "<<", ft_strlen(tmp)));
+		begin = ft_strlen(lst->str_aux) - \
+		ft_strlen(ft_strnstr(tmp, "<<", ft_strlen(tmp)));
 		if ((ft_strnstr(tmp, "<<", ft_strlen(tmp)) == NULL))
 			break ;
 		else
 		{
-			while (tmp[begin] == '<')
-				begin++;
-			while (ft_isspace(tmp[begin]))
-				begin++;
-			lst->herename = (char *) malloc(sizeof (char) * ((ft_strlen(tmp) - begin) + 1));
-			len = 0;
-			while (tmp[begin])
-			{
-				if (quotes == 0 && ft_isspace(tmp[begin]))
-					break ;
-				if (quotes == 0)
-				{
-					d_comma(tmp[begin], &quotes);
-					if (quotes > 0)
-						begin++;
-				}
-				else
-				{
-					d_comma(tmp[begin], &quotes);
-					if (quotes == 0)
-						begin++;
-				}
-				if (tmp[begin] != '\0')
-				{
-					lst->herename[len] = tmp[begin];
-					begin++;
-					len++;
-				}
-			}
-			lst->herename[len] = '\0';
-			// revisar aqui
-			//ft_set_strpntr(&tmp, &lst->str_aux, &lst->str_line, 2);
-			// hay que limpiar la cadena para quitar los elementos de heredoc que se han realizado
-			//ft_clear_str(tmp, lst); // puede fallar aqui....
-			lst->str_save = one_erase_redir(lst->str_save);
-			lst->str_aux = one_erase_redir(lst->str_aux);
+			hdoc_parse(lst, tmp, &begin, &quotes);
 			tmp = lst->str_save;
 		}
-		//lst->herename = ft_eofcatch(&lst->here_num, &lst->str_line);
 		heredoc_opener(lst->herename);
-		//hacer free de herename
 	}
+}
+	/*hacer free de herename*/
+
+void	hdoc_parse(t_lst *lst, char *tmp, int *begin, int *quotes)
+{
+	while (tmp[(*begin)] == '<')
+		(*begin)++;
+	while (ft_isspace(tmp[(*begin)]))
+		(*begin)++;
+	lst->herename = (char *) malloc(sizeof (char) * \
+	((ft_strlen(tmp) - (*begin)) + 1));
+	get_herename(lst, tmp, begin, quotes);
+	lst->str_save = one_erase_redir(lst->str_save);
+	lst->str_aux = one_erase_redir(lst->str_aux);
+}
+
+void	get_herename(t_lst *lst, char *tmp, int *begin, int *quotes)
+{
+	int		len;
+
+	len = 0;
+	while (tmp[(*begin)])
+	{
+		if ((*quotes) == 0 && ft_isspace(tmp[(*begin)]))
+			break ;
+		look_for_begin(tmp, begin, quotes);
+		if (tmp[(*begin)] != '\0')
+		{
+			lst->herename[len] = tmp[(*begin)];
+			one_more2(begin, &len);
+		}
+	}
+	lst->herename[len] = '\0';
 }
 
 char	*ft_eofcatch(int *hd_num, char **dat_line)
@@ -128,60 +103,3 @@ char	*ft_eofcatch(int *hd_num, char **dat_line)
 	*hd_num += 1;
 	return (eof);
 }
-
-void	ft_hdc_prepareln(t_lst *lst)
-{
-	int		pos;
-	char	*tmp;
-
-	pos = (lst->here_num + 1);
-	tmp = lst->str_save;
-	while ((--pos > 1) && *tmp != '\0')
-	{
-		tmp = ft_strnstr(tmp, "\\", ft_strlen(tmp));
-		*tmp = ' ';
-	}
-	tmp = ft_strnstr(tmp, "\\", ft_strlen(tmp));
-	if (tmp == NULL)
-		return ;
-	tmp++;
-	while (*tmp == '*' && *tmp != '\0')
-		*tmp++ = ' ';
-	lst->str_line = lst->str_save;
-}
-
-int	heredoc_opener(char *file)
-{
-	char	*line;
-	int		fd;
-
-	fd = open(ft_strjoin(TMP_PATH, file), O_CREAT | O_WRONLY | O_TRUNC, 0666);
-	g_ms->state = H_DOC_CMD;
-	ft_set_signal();
-	if ((fd < 0) || (!(g_ms->state == H_DOC_CMD)))
-		return (-1);
-	line = readline("> ");
-	while ((line && !(precise_cmp(line, file, ft_strlen(file)))) \
-	&& (g_ms->state == H_DOC_CMD))
-	{
-		if (!((file[0] == '\'') || (file[0] == '\"')))
-			line = expand_vars(line);
-		ft_putstr_fd(line, fd);
-		write(fd, "\n", 1);
-		free(line);
-		line = NULL;
-		line = readline("> ");
-	}
-	free(line);
-	close(fd);
-	return (0);
-}
-/* signal main
-  while (1)
-  {
-   line = readline("$> ");
-   if (!line || !ft_strncmp(line, eof, ft_maxlen(line, eof)))
-    exit(0);
-   else
-    write_line_on_hdoc(line, node->fdi);
-  }*/
